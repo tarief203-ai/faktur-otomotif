4fb043d676cd98c317b212e7c307f904724322e9
tarief203-ai <tarief203@gmail.com>
2025-12-19 20:35:15 +0700

    feat: menambahkan controller dan route pemilik

---
+++ app/Http/Controllers/PemilikController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; // Penting untuk query database
+
+class PemilikController extends Controller
+{
+    // Fungsi ini yang tadi dianggap tidak ada (undefined)
+    public function index()
+    {
+        // Mengambil semua data dari tabel pemilik
+        $pemiliks = DB::table('pemilik')->get();
+        
+        // Mengirim data ke view pemilik/index.blade.php
+        return view('pemilik.index', compact('pemiliks'));
+    }
+
+    public function create()
+    {
+        return view('pemilik.create');
+    }
+
+    public function store(Request $request)
+    {
+        DB::table('pemilik')->insert([
+            'id_pemilik' => $request->id_pemilik,
+            'nama' => $request->nama,
+            'alamat' => $request->alamat,
+            'kode_pos' => $request->kode_pos
+        ]);
+        return redirect('/pemilik');
+    }
+
+    public function edit($id)
+    {
+        $pemilik = DB::table('pemilik')->where('id_pemilik', $id)->first();
+        return view('pemilik.edit', compact('pemilik'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        DB::table('pemilik')->where('id_pemilik', $id)->update([
+            'nama' => $request->nama,
+            'alamat' => $request->alamat,
+            'kode_pos' => $request->kode_pos
+        ]);
+        return redirect('/pemilik');
+    }
+
+    public function destroy($id)
+    {
+        DB::table('pemilik')->where('id_pemilik', $id)->delete();
+        return redirect('/pemilik');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PemilikController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Halaman Utama & Tambah
+Route::get('/pemilik', [PemilikController::class, 'index']);
+Route::get('/pemilik/create', [PemilikController::class, 'create']);
+Route::post('/pemilik/store', [PemilikController::class, 'store']);
+
+// Ubah & Hapus
+Route::get('/pemilik/edit/{id}', [PemilikController::class, 'edit']);
+Route::post('/pemilik/update/{id}', [PemilikController::class, 'update']);
+Route::get('/pemilik/delete/{id}', [PemilikController::class, 'destroy']);
