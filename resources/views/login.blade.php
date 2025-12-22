340cba1078e94ee865346f2eab26c2f509c8db57
tarief203-ai <tarief203@gmail.com>
2025-12-22 21:06:18 +0700

    memperbaiki sedikit halaman login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - The Vos</title>
+    <title>Login - Pros pect</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <style>
