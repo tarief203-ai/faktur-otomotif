2694e18f966dc484d65f97b77138db85636af43f
tarief203-ai <tarief203@gmail.com>
2025-12-31 17:32:31 +0700

    memperbaikin login agar pak rizal bisa melihat data login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,39 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
     /**
-     * Seed the application's database.
+     * Akun Login untuk Dosen:
+     * Admin: admin@test.com | pw: admin123
+     * Staff: staff@test.com | pw: staff123
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // 1. Akun Admin
+        User::updateOrCreate(
+            ['email' => 'admin@test.com'],
+            [
+                'name' => 'Administrator',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // 2. Akun Staff
+        User::updateOrCreate(
+            ['email' => 'staff@test.com'],
+            [
+                'name' => 'Staff Operasional',
+                'password' => Hash::make('staff123'),
+                'role' => 'staff',
+            ]
+        );
     }
 }
